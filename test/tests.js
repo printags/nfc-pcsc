aa0638e135844e327a14cf65bc9331e7baae3cea
NefiteTifall | Nicolas <nefitetifall@gmail.com>
2025-05-16 11:34:43 +0200

    Replace @pokusew/pcsclite with @printags/node-pcsclite and setup GitHub Packages publishing

--- test/tests.js
+++ test/tests.js
@@ -3,9 +3,9 @@
 import test from 'ava';
 import mock from 'mock-require';
 
-// mock @pokusew/pcsclite to allow to simulate cards
+// mock @printags/node-pcsclite to allow to simulate cards
 import pcscliteMock from './helpers/pcsclite-mock';
-mock('@pokusew/pcsclite', pcscliteMock);
+mock('@printags/node-pcsclite', pcscliteMock);
 const NFC = require('../src/NFC').default;
 
 
